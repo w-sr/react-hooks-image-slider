e82f9a20e2828221340852bbc13bf581ea0b4ac6
ares <ares@aress-iMac.local>
2020-04-28 04:46:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Slider from './components/slider'
+import Slider from './components/Slider'
 import images from './images'
 
 ReactDOM.render(<Slider images={images} />, document.querySelector('.main'))
